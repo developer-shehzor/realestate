b2ddadfdeddf560510a2352af885c6ff5e365467
ShehzorMemon <Shezymemon0180@gmail.com>
2024-05-08 22:44:46 +0500

    Completed delete user listing functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -130,6 +130,23 @@ export default function Profile() {
       setShowListingsError(true)
     }
   }
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listings/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json()
+      if (data.success === false) {
+        console.log(data.message)
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing.id !== listingId))
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   
 
   return (
@@ -202,7 +219,7 @@ export default function Profile() {
           </Link>
   
           <div className='flex flex-col items-center'>
-            <button className='text-red-700 uppercase'>Delete</button>
+            <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
             <button className='text-green-700 uppercase'>Edit</button>
           </div>
         </div>
